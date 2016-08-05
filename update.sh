f2a705ca149a0b0d91e8ffa8ba43d73d24639546
Anna Ladoshkina <webdev@foralien.com>
2016-08-05 19:07:21 +0300

    Email template hack

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 rsync -avzr --exclude='.DS_Store' ./wp-content/themes/mailer/issues-themes/teplitsa  ./wp-content/uploads/wysija/themes/
+rsync -avz  ./wp-content/themes/mailer/issues-themes/email_template.html  ./wp-content/plugins/wysija-newsletters/tools/templates/newsletter/email/email_template.html
