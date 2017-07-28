644eedc43af1c11c02d83faa0ccb0466aa835672
DenisCherniatev <denis.cherniatev@gmail.com>
2017-07-29 00:27:48 +0200

    * footer menu markup added

--- wp-content/themes/mailer/footer.php
+++ wp-content/themes/mailer/footer.php
@@ -4,8 +4,14 @@
 
 <div class="container">	
 	
-	<div class="footer-content">Теплица социальных технологий</div>
-	
+	<div class="footer-content">
+    
+    Теплица социальных технологий
+    
+    <?php wp_nav_menu(array('menu' => 'pd-footer-links', 'container' => false, 'menu_class' => 'pd-footer-links')); ?>
+    
+    </div>
+    
 </div>
 </footer>
 
