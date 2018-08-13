7b194f651a3f62458091513b6488e560aa5699ed
DenisCherniatev <denis.cherniatev@gmail.com>
2018-08-13 22:40:56 +0200

    * fix

--- wp-content/themes/mailer/email_tst_footer.php
+++ wp-content/themes/mailer/email_tst_footer.php
@@ -1,5 +1,5 @@
 
-<span style="display: block; border-top: 1px solid #dfe3e6; line-height: 48px; padding-top: 22px; margin-top: -17px;">
+<span style="display: block; border-top: 1px solid #dfe3e6; line-height: 48px; padding-top: 22px; margin-top: -7px;">
     <a href="https://vk.com/teplitsast" style="vertical-align: middle;text-decoration:none;border:none;float:left;margin-right:6px;"><img src="<?php echo get_template_directory_uri()?>/assets/img/social-vk.png" style="border:none;width:32px;height:32px;" /></a>
     <a href="https://www.facebook.com/TeplitsaST" style="vertical-align: middle;text-decoration:none;border:none;float:left;margin-right:6px;"><img src="<?php echo get_template_directory_uri()?>/assets/img/social-fb.png" style="border:none;width:32px;height:32px;" /></a>
     <a href="hhttps://twitter.com/TeplitsaST" style="vertical-align: middle;text-decoration:none;border:none;float:left;margin-right:6px;"><img src="<?php echo get_template_directory_uri()?>/assets/img/social-tw.png" style="border:none;width:32px;height:32px;" /></a>
@@ -8,7 +8,7 @@
     <span style="display: block;float:none;clear:both;line-height: 1px;"></span>
 </span>
 
-<span style="display: block; text-align:center;text-transform:uppercase;margin-top:0px;font-size:12px;color:#7c8284;">
+<span class="tst-footer-short-description" style="display: block;text-align:center;text-transform:uppercase;margin-top:0px;font-size:12px;color:#7c8284;">
 Теплица социальных технологий. Наши лучшие материалы для вас.
 </span>
 
