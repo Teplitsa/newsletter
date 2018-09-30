6c567f8dbba93d1d05b9239682ce9c6ccffb173f
DenisCherniatev <denis.cherniatev@gmail.com>
2018-10-01 00:30:51 +0200

    * fix

--- wp-content/themes/mailer/email_tst_header.php
+++ wp-content/themes/mailer/email_tst_header.php
@@ -1,6 +1,6 @@
 <div style="margin-bottom: 10px;">
 	<a href="https://te-st.ru" style="float:left;text-decoration:none;border:none;"><img src="<?php echo get_template_directory_uri()?>/assets/img/tst-logo-120x66.png" style="border:none;width:120px;height:66px;" /></a>
-	<div style="float:left;border-left: 1px solid #dfe3e6;height:41px;margin-left:30px;padding-left: 30px;padding-top:25px;"><a href="https://te-st.ru" style="color:#219665;font-size:14px;line-height:16px;font-weight:bold;text-transform:uppercase;vertical-align:middle;text-decoration:none;">te-st.ru</a></div>
+	<div style="float:left;border-left: 1px solid #dfe3e6;height:44px;margin-left:30px;padding-left: 30px;padding-top:22px;"><a href="https://te-st.ru" style="color:#219665;font-size:14px;line-height:16px;font-weight:bold;text-transform:uppercase;vertical-align:middle;text-decoration:none;">te-st.ru</a></div>
 	<div style="float:none;clear:both;"></div>
 </div>
 	
